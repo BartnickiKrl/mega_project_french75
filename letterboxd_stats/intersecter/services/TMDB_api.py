1c8b826caa08079abda5c1287b3bb8b3d609b98a
BartnickiKrl <karoljedi09@gmail.com>
2026-04-18 10:57:10 +0000

    final_database_update

--- letterboxd_stats/intersecter/services/TMDB_api.py
+++ letterboxd_stats/intersecter/services/TMDB_api.py
@@ -30,6 +30,40 @@ def get_movie_info(title):
         "tytul": title_full,
         "rok": year,
         "gatunki": genres,
+        "rezyser": director }
+
+import requests
+
+API_KEY = "f4ee279337d6012678be52531803c039"
+BASE_URL = "https://api.themoviedb.org/3"
+IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+
+def get_movie_info(title):
+
+    search_url = f"{BASE_URL}/search/movie"
+    search_params = {"api_key": API_KEY, "query": title}
+    search_res = requests.get(search_url, params=search_params).json()
+
+    if not search_res['results']:
+        return "Nie znaleziono filmu."
+
+    movie_id = search_res['results'][0]['id']
+
+    detail_url = f"{BASE_URL}/movie/{movie_id}"
+    detail_params = {"api_key": API_KEY, "append_to_response": "credits"}
+    movie = requests.get(detail_url, params=detail_params).json()
+
+    title_full = movie['title']
+    genres = [g['name'] for g in movie['genres']]
+    year = movie['release_date'][:4] if movie['release_date'] else "N/A"
+
+    director = next((person['name'] for person in movie['credits']['crew']
+                     if person['job'] == 'Director'), "Nieznany")
+
+    return {
+        "tytul": title_full,
+        "rok": year,
+        "gatunki": genres,
         "rezyser": director
     }
 
@@ -40,7 +74,3 @@ def get_poster(title):
     movie_data = search_res['results'][0]
     poster_path = movie_data.get('poster_path')
     return f"{IMAGE_BASE_URL}/{poster_path}"
-
-
-
-
--- letterboxd_stats/intersecter/services/database_update.py
+++ letterboxd_stats/intersecter/services/database_update.py
@@ -1,7 +1,6 @@
 import concurrent.futures
 
 from models import Directors, Genres, LetterboxdUsers, Movies
-from numpy import linspace
 from parser import get_titles, get_watchlist_len
 from scraper import LetterboxdClient
 from TMDB_api import get_movie_info
@@ -15,7 +14,7 @@ def manage_scrapping(nicnames:list):
         pages = get_watchlist_len(r)
         films_first = get_titles(r)
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            films_titles = executor.map(LetterboxdClient.fetch_watchlist, user, linspace(1, pages))
+            films_titles = executor.map(LetterboxdClient.fetch_watchlist, user, range(1, pages+1))
         movies[user] = films_first + films_titles
         x.extend(movies[user])
 
