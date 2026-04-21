b913e806b90593e9d144bd88c073a922b46cf410
Michał <mmajdzik11@gmail.com>
2026-04-21 16:15:05 +0000

    database_test_changes#4(working)

--- letterboxd_stats/intersecter/services/files_sql/top_movie.sql
+++ letterboxd_stats/intersecter/services/files_sql/top_movie.sql
@@ -1,15 +1,5 @@
 
--- select m.Title
--- from (SELECT m.Title,m.id   
---     FROM intersecter_Movies m
---     inner join intersecter_LetterboxdUsers u on u.MovieID = m.id
---     INNER JOIN intersecter_Genres g ON m.GENREID = g.NAME
---     WHERE u.NickName in ({users_list}) and
---     WHERE g.Name = {selected_genre}) movie
--- inner join intersecter_LetterboxdUsers u on u.MovieID = movie.id
--- group by m.Title
--- order by count(u.Name) desc
--- Limit {n}
+-- nalezy stosowac tabele przejsciowe!!!!!
 
 select m.Title, m.Year, d.Name
 FROM intersecter_Movies as m
--- letterboxd_stats/intersecter/services/intersecter.py
+++ letterboxd_stats/intersecter/services/intersecter.py
@@ -14,16 +14,26 @@ def SQL_executor(sql_file:str, **sql_params):
         template = f.read()
 
     final_sql = template.format(**sql_params)
+    print(final_sql)
 
     with connection.cursor() as cursor:
         cursor.execute(final_sql)
+        #result = cursor.fetchall()
+        result = cursor.fetchone() #robimy fetchone() bo i tak mamy LIMIT 1
+        #fetchall() zwraca liste krotek (wierszy) [()] wiec potem w views jak robimy
+        #[...,...,...] = Intersecter(...) to nie potrafi przypisac wyniku do trzech zmiennych
+        #ewentualnym rozwiazaniem byloby tez wyciagniecie pierwsze elementu z wyniku Interceter(...)
 
-    return cursor.fetchall()
+    #return cursor.fetchall() - nie moze byc tutaj tego bo cursor został zamkniety po wyjsciu z with
+    return result
 
 
 def Intersect(users, genre, counter=0):
-    users = ", ".join([f"'{u}'" for u in users])
-    genre = f"'{genre}'"
+    users = ", ".join([f"'{u}'" for u in users]) #wartosci w "" sa traktowane w sql jako kolumny
+    #trzeba bylo zamienic na ''
+    genre = f"'{genre.capitalize()}'"
+    #trzeba bylo dac .capitalize() bo genre bylo z malej litery a w database jest z duzej
     the_movie = SQL_executor(SQL_FILES[0], users_list=users, n=counter, selected_genre=genre)
 
+    #sprawdzic potem !the_movie - sytuacja gdzie nic nie wyjdzie
     return the_movie
