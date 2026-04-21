0ac7eaf6cd88d5b9ad0962cb0c3f1886d4ce9662
BartnickiKrl <karoljedi09@gmail.com>
2026-04-21 12:47:49 +0000

    intersecter.py

---
+++ letterboxd_stats/intersecter/services/intersecter.py
@@ -0,0 +1,26 @@
+from django.db import connection
+
+SQL_DIR = "files_sql/"
+
+SQL_FILES = [
+    SQL_DIR + "top_movie.sql"]
+
+
+def SQL_executor(sql_file:str, **sql_params):
+
+    with open(sql_file) as f:
+        template = f.read()
+
+    final_sql = template.format(**sql_params)
+
+    with connection.cursor() as cursor:
+        cursor.execute(final_sql)
+
+    return cursor.fetchall()
+
+
+def Intersect(users, genre, counter=0):
+
+    the_movie = SQL_executor(SQL_FILES[0], users_list=users, n=counter, selected_genre=genre)
+
+    return the_movie
--- letterboxd_stats/intersecter/views.py
+++ letterboxd_stats/intersecter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import *
 from .services.database_update import manage_scrapping
+from .services.intersecter import Intersect
 from .services.TMDB_api import get_poster
 
 
@@ -9,21 +10,50 @@ def home(request):
     if request.method == "POST":
         nicknames = request.POST.getlist('nickname[]')
         manage_scrapping(nicknames=nicknames)
+
         genre = request.POST.get('genre')
-        first = Movies.objects.first()
+
+        # Inicjalizacja słownika w sesji
+        genres_counter = request.session.get('genres_counter', {})
+        genres_counter[genre] = 0
+
+        # Zapis do sesji
+        request.session['genres_counter'] = genres_counter
+        request.session['last_nicknames'] = nicknames
+
+        [title, year, director] = Intersect(nicknames, genre)
 
         print(f"{nicknames}, {genre}")
-        return render(request, 'intersecter/movie.html', {"title": first.Title,
-                                                         "year": first.Year,
-                                                         "poster_url": get_poster(first.Title)})
+        return render(request, 'intersecter/movie.html', {
+            "title": title,
+            "year": year,
+            "director": director,
+            "poster_url": get_poster(title)
+        })
     return render(request, 'intersecter/home.html')
 
 def movie(request):
     if request.method == "POST":
         genre = request.POST.get('genre')
+
+        nicknames = request.session.get('last_nicknames', [])
+        genres_counter = request.session.get('genres_counter', {})
+
+        new_count = genres_counter.get(genre, -1) + 1
+        genres_counter[genre] = new_count
+
+        # Aktualizacja sesji
+        request.session['genres_counter'] = genres_counter
+        request.session.modified = True
+
+        [title, year, director] = Intersect(nicknames, genre, counter=new_count)
+
         print(f"nowy gatunek {genre}")
-        second = Movies.objects.all()[1]
-        return render(request, 'intersecter/movie.html', {"title": second.Title,
-                                                         "year": second.Year,
-                                                         "poster_url": get_poster(second.Title)})
+        return render(request, 'intersecter/movie.html', {
+            "title": title,
+            "year": year,
+            "director": director,
+            "poster_url": get_poster(title),
+            "genre": genre
+        })
     return render(request, 'intersecter/movie.html')
